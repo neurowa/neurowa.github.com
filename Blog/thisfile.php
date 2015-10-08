8a23aecf9715e30af4b4d738d655df787f91ad35
neurowa <15484013@student.curtin.edu.au>
2015-10-08 16:20:18 +0800

    Continues

--- Blog/thisfile.php
+++ Blog/thisfile.php
@@ -1,13 +1,10 @@
 <?php
     $file = '/Blog/messages.json';
     $messages = json_decode(file_get_contents($file),true);
-    if(empty($messages)){
-        $messages = array();
-    }
-
+	
     if(isset($_POST['username'])&&isset($_POST['mssg'])) {  //save settings
         $messages[] = $_POST; //remember to sanitize
-        file_put_contents($file,json_encode($messages));
+        file_put_contents($file,json_encode($messages),FILE_APPEND);
         exit;
     }elseif(isset($_POST['read'])&&$_POST['read']=='messages'){
         echo json_encode($messages);
