c4738d5e82966680062fd7420bb77364c59d26f8
neurowa <15484013@student.curtin.edu.au>
2015-10-08 15:49:27 +0800

    Blog comments attempt

---
+++ Blog/thisfile.php
@@ -0,0 +1,18 @@
+<?php
+    $file = 'messages.json';
+    touch($file);       //create file if not exist
+    $messages = json_decode(file_get_contents($file),true);
+    if(empty($messages)){
+        $messages = array();
+    }
+
+    if(isset($_POST['username'])&&isset($_POST['mssg'])) {  //save settings
+        $messages[] = $_POST; //remember to sanitize
+        file_put_contents($file,json_encode($messages));
+        exit;
+    }elseif(isset($_POST['read'])&&$_POST['read']=='messages'){
+        echo json_encode($messages);
+        exit;
+    }
+
+?>
