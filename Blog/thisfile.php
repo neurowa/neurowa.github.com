dff122eab5fcb7f45e68d0aa77f2f81a8a96e8f5
neurowa <15484013@student.curtin.edu.au>
2015-10-09 17:22:22 +0800

    CommTest2

--- Blog/thisfile.php
+++ Blog/thisfile.php
@@ -1,21 +1,8 @@
 <?php
-    $messages = json_decode(file_get_contents('messages.json'),true);
-	
+	$file = 'messages.json';
+	$messages = json_decode(file_get_contents($file),true);
     if(isset($_POST['username'])&&isset($_POST['mssg'])) {  //save settings
-        $message = new StdClass();
-		$message->username = htmlentities($_POST['username']);
-		$message->mssg = htmlentities($_POST['mssg']);
-		
-		$messages[] = $message; //remember to sanitize
-		$fp = fopen('messages.json','w+');
-		fwrite($fp, json_encode($messages));
-		fclose($fp));
-		
-        //file_put_contents($file,json_encode($messages),FILE_APPEND);
+        $messages[] = $_POST; //remember to sanitize
+        file_put_contents($file,json_encode($messages));
         exit;
-		}elseif(isset($_POST['read'])&&$_POST['read']=='messages'){
-		   echo json_encode($messages);
-		   exit;
-		}
-
 ?>
