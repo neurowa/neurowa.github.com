8eadbcd5f7793055f2ce9a04e6e67c1d41aa3533
neurowa <15484013@student.curtin.edu.au>
2015-10-09 17:28:26 +0800

    Assrt

--- Blog/thisfile.php
+++ Blog/thisfile.php
@@ -1,7 +1,7 @@
 <?php
 	$file = './messages.json';
 	$messages = json_decode(file_get_contents($file),true);
-    if(isset($_POST['username'])&&isset($_POST['mssg'])) {  //save settings
+    if(isset($_POST['Username'])&&isset($_POST['Message'])) {  //save settings
         $messages[] = $_POST; //remember to sanitize
         file_put_contents($file,json_encode($messages));
         exit;
