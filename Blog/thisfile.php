93c1f982b5ee4010d81e1ae9a8398bcf277b540d
neurowa <15484013@student.curtin.edu.au>
2015-10-09 09:51:20 +0800

    AS

--- Blog/thisfile.php
+++ Blog/thisfile.php
@@ -1,13 +1,13 @@
 <?php
-    $messages = json_decode(file_get_contents("messages.json"),true);
+    $messages = json_decode(file_get_contents('messages.json'),true);
 	
     if(isset($_POST['username'])&&isset($_POST['mssg'])) {  //save settings
         $message = new StdClass();
-		$message->username = htmlentities($_POST["username"]);
-		$message->mssg = htmlentities($_POST["mssg"]);
+		$message->username = htmlentities($_POST['username']);
+		$message->mssg = htmlentities($_POST['mssg']);
 		
 		$messages[] = $message; //remember to sanitize
-		$fp = fopen("messages.json","w+");
+		$fp = fopen('messages.json','w+');
 		fwrite($fp, json_encode($messages));
 		fclose($fp));
 		
