fcfae0fdd98688e156e91936802f96baaa550eee
neurowa <15484013@student.curtin.edu.au>
2015-11-10 08:25:53 +0800

    Blog clean-up

--- Blog/thisfile.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$file = './messages.json';
-	$messages = json_decode(file_get_contents($file),true);
-    if(isset($_POST['Username'])&&isset($_POST['Message'])) {  //save settings
-        $messages[] = $_POST; //remember to sanitize
-        file_put_contents($file,json_encode($messages));
-        exit;
-?>
