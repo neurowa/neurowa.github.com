1271286ac7a93ff1dc33d4f905896b7d5012ea31
Andrew <15484013@student.curtin.edu.au>
2016-10-27 17:52:21 +0800

    Side test

--- Blog2/wp-content/themes/Synapse/sidebar.php
+++ Blog2/wp-content/themes/Synapse/sidebar.php
@@ -8,8 +8,6 @@
  */
 ?>
 
-<?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
+
 	<aside id="secondary" class="span4 sidebar widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- .sidebar .widget-area -->
-<?php endif; ?>
