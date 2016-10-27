b55fcad2a419c9455212bcbc5e1a0cc67622abbe
Andrew <15484013@student.curtin.edu.au>
2016-10-27 17:34:20 +0800

    aa

---
+++ Blog2/wp-content/themes/Synapse/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+    </div> <!-- End Container -->
+
+<!-- Footer Area-->
+<div id="Footer"></div>	
+<script>$('#Footer').load('../MFooter.htm');</script>	
+<!-- End Footer --> 
+
+    <!-- Scroll to Top -->  
+    <div id="toTop" class="hidden-phone hidden-tablet">Back to Top</div>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
