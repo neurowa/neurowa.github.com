cc2b03dc858d361ec5a4294e62dbb506fc6d5252
Andrew <15484013@student.curtin.edu.au>
2016-10-27 18:01:15 +0800

    Test

--- Blog2/wp-content/themes/Synapse/page.php
+++ Blog2/wp-content/themes/Synapse/page.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="span8 content-area">
 	<main id="main" class="site-main" role="main">
 		<?php
 		// Start the loop.
