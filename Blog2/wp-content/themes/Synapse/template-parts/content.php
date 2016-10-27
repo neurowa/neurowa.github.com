43b4c0b65b5daa0067f92e3da6271b5284ddb0b5
Andrew <15484013@student.curtin.edu.au>
2016-10-27 20:29:43 +0800

    ss

--- Blog2/wp-content/themes/Synapse/template-parts/content.php
+++ Blog2/wp-content/themes/Synapse/template-parts/content.php
@@ -21,7 +21,7 @@
 
 	<?php twentysixteen_post_thumbnail(); ?>
 
-	<div class="span6 entry-content" style="background-color:#f8f8f8;float:left;margin-left:0px">
+    </br><div class="span6 entry-content" style="background-color:#f8f8f8;float:left;margin-left:0px">
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
