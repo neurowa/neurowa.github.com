69f438a35a707712bb37d236e2d118b5b41a4fbf
Andrew <15484013@student.curtin.edu.au>
2016-10-30 18:16:17 +0800

    sdf

--- Blog2/wp-content/themes/twentysixteen/footer.php
+++ Blog2/wp-content/themes/twentysixteen/footer.php
@@ -54,6 +54,17 @@
 	</div><!-- .site-inner -->
 </div><!-- .site -->
 
+    </div> <!-- End Container -->
+
+<!-- Footer Area-->
+<div id="Footer"></div>	
+<script>$('#Footer').load('../MFooter.htm');</script>	
+<!-- End Footer --> 
+
+    <!-- Scroll to Top -->  
+    <div id="toTop" class="hidden-phone hidden-tablet">Back to Top</div>
+    
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- Blog2/wp-content/themes/twentysixteen/header.php
+++ Blog2/wp-content/themes/twentysixteen/header.php
@@ -12,6 +12,40 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
+<base href="../../../../">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Synapse</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- CSS
+================================================== -->
+<link href='../../../../css/font.css' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="../../../../css/bootstrap.css">
+<link rel="stylesheet" href="../../../../css/bootstrap-responsive.css">
+<link rel="stylesheet" href="../../../../css/prettyPhoto.css" />
+<link rel="stylesheet" href="../../../../css/custom-styles.css">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" href="css/style-ie.css"/>
+<![endif]--> 
+
+<!-- Favicons
+================================================== -->
+<link rel="shortcut icon" href="../../../../img/favicon.ico">
+<link rel="apple-touch-icon" href="../../../../img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../../../../img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../../../../img/apple-touch-icon-114x114.png">
+
+<!-- JS
+================================================== -->
+<script src="../../../../js/jquery-1.8.3.min.js"></script>
+<script src="../../../../js/jquery.easing.1.3.js"></script>
+<script src="../../../../js/bootstrap.js"></script>
+<script src="../../../../js/jquery.prettyPhoto.js"></script>
+<script src="../../../../js/jquery.quicksand.js"></script>
+<script src="../../../../js/jquery.custom.js"></script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -22,6 +56,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+    
+	<div class="color-bar-1"></div>
+    <div class="color-bar-2 color-bg"></div>
+    
+    <div class="container main-container">
+    
+<!-- Begin Header -->
+<div id="Header"></div>	
+<script>$('#Header').load('../MHeader.htm');</script>
+    
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
